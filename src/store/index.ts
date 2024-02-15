04a507fba1a85a2530689cf1aa912903a30c3ac7
yuran1811 <trieuvanbd123@gmail.com>
2024-02-15 14:17:53 +0700

    feat: ✨ add Zustand for state management

---
+++ src/store/index.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface SettingsType {
+  alwaysOnTop: boolean;
+  isMinimalUI: boolean;
+  maxCharToShow: number;
+  mergeDuplicates: boolean;
+}
+
+type ThemeVariants = 'light' | 'dark' | 'violet' | 'slate';
+
+interface StoreType {
+  settings: SettingsType;
+  setSettings: (settings: SettingsType) => void;
+
+  theme: ThemeVariants;
+  setTheme: (theme: ThemeVariants) => void;
+}
+
+export const useStore = create<StoreType>()(
+  persist(
+    (set) => ({
+      settings: {
+        alwaysOnTop: true,
+        isMinimalUI: false,
+        maxCharToShow: 5,
+        mergeDuplicates: true,
+      },
+      setSettings: (settings) => set({ settings }),
+
+      theme: 'dark',
+      setTheme: (theme) => set({ theme }),
+    }),
+    { name: 'app-storage' },
+  ),
+);
